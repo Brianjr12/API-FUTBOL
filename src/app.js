da2bcf5dc8aed684a6abbb8c94f55d8ffb405eae
Brian Ojeda <brianojedadiaz1204@gmail.com>
2023-10-07 20:07:50 -0500

    file configuration was modified

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import morgan from 'morgan'
+import router from './routes/appRoutes.js'
+
 const app = express()
 const port = 3000
+app.use(morgan("dev"))
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
+app.use(router)
